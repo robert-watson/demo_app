9d8187d8ebc48b4ec13f0173a10f44d40a8f0cb8
robert-watson <robert.watson@sportsdatahub.com>
2013-09-04 16:40:28 -0700

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+DemoApp::Application.config.secret_token = '02674b43f8945618ac82fd07f1151699d37cc4278055122f090752dd1b249cae6b7ed9a0f421b9f6e3566720b24deb9fe82eab945b8d38ac76c166ac5c2b2e9c'
